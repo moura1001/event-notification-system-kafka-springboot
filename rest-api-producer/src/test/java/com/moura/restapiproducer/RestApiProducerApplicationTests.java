ab891dc0654847fc720ce5fce08b5babfd6d67a0
moura1001 <gmouraneto01@gmail.com>
2024-03-25 19:59:18 -0300

    config: setup do projeto + docker-compose com serviços do kafka, postgres e pgadmin

---
+++ rest-api-producer/src/test/java/com/moura/restapiproducer/RestApiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.restapiproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
